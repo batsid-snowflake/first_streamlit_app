3c223ba60fe180571cb0adbfe77de9c45d184372
batsid-snowflake <146960541+batsid-snowflake@users.noreply.github.com>
2023-10-04 17:54:20 +0300

    Added Breakfast Menu & Options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+steamlit.text('Hard-Boiled Free-Range Eggs')
