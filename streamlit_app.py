948d570ff0d4b7e56053f70812f93c8760bd6396
batsid-snowflake <146960541+batsid-snowflake@users.noreply.github.com>
2023-10-12 15:23:23 +0300

    Make displayed data look nice

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())  # Just writes the json data to the screen
+# Take the json string from the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Present on the screen as a Table
+streamlit.dataframe(fruityvice_normalized)
